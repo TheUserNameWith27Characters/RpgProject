01ff61918454933a463b8c1ddf0b0fbb092fa599
TheUserNameWith27Characters <TheUserNameWith27Characters@users.noreply.github.com>
2015-05-31 11:07:21 -0600

    Create Sounds.py

---
+++ Sounds.py
@@ -0,0 +1,5 @@
+from Fonts_And_Colours import *
+gameOverSound=pygame.mixer.Sound(path.join("Sounds","Game_Over.wav"))
+playerHitSound=pygame.mixer.Sound(path.join("Sounds","Player_Hit.wav"))
+playerHitSound2=pygame.mixer.Sound(path.join("Sounds","Player_Hit2.wav"))
+menuSelect=pygame.mixer.Sound(path.join("Sounds","menu_select.wav"))
